ac621d67f014d7fecbd9259e012079daf3b2fcd9
AhmadTaufiqR <ahmadtaufiqridhoi@gmail.com>
2025-04-19 05:50:44 +0700

    add: permission location on ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps // <--- Tambahkan ini
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,11 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    GMSServices.provideAPIKey("AIzaSyB0SLYtTFclknN7lXyVS10a23ps4K11AXA") // <--- Tambahkan ini
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
